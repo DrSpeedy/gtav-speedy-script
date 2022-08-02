b55212e314d48ff1f09228f5d5716714002236f3
Brian Wilson <brian@wiltech.org>
2022-08-02 03:40:11 -0400

    Refactor super run so it is more like rest of code

--- store/Speedy/superrun_options.lua
+++ store/Speedy/superrun_options.lua
@@ -7,82 +7,83 @@ local sr_base_speed = 10
 local sr_accel_mult = 0.77
 local sr_brake_mult = 0.66
 
-local function DoSuperRun(toggle)
-	local is_super_running = false
+local bSuperRunEnabled = false
 
+local function DoSuperRun(toggle)
+	bSuperRunEnabled = toggle
+    local is_super_running = false
 	-- Our current speed multiplier, this will be increased as the player speeds up
 	local speed_mult = sr_base_speed
 
 	util.create_tick_handler(function()
-		if (toggle) then
-			-- Local variables needed updated each tick
-			local player = PLAYER.PLAYER_PED_ID()
-			local velocity = ENTITY.GET_ENTITY_VELOCITY(player)
-			local direction = ENTITY.GET_ENTITY_FORWARD_VECTOR(player)
-			local sprinting = TASK.IS_PED_SPRINTING(player)
-			local ragdoll = PED.IS_PED_RAGDOLL(player)
-			local falling = PED.IS_PED_FALLING(player)
-			local swimming = PED.IS_PED_SWIMMING(player)
-			local parastate = PED.GET_PED_PARACHUTE_STATE(player)
-			local grounddistance = ENTITY.GET_ENTITY_HEIGHT_ABOVE_GROUND(player)
+        -- Local variables needed updated each tick
+        local player = PLAYER.PLAYER_PED_ID()
+        local velocity = ENTITY.GET_ENTITY_VELOCITY(player)
+        local direction = ENTITY.GET_ENTITY_FORWARD_VECTOR(player)
+        local sprinting = TASK.IS_PED_SPRINTING(player)
+        local ragdoll = PED.IS_PED_RAGDOLL(player)
+        local falling = PED.IS_PED_FALLING(player)
+        local swimming = PED.IS_PED_SWIMMING(player)
+        local parastate = PED.GET_PED_PARACHUTE_STATE(player)
+        local grounddistance = ENTITY.GET_ENTITY_HEIGHT_ABOVE_GROUND(player)
 
-			if (PAD.IS_CONTROL_PRESSED(2, keys['RB']) and sprinting and not (parastate == 0)) then
-				if not (PED.IS_PED_IN_ANY_VEHICLE(player, true) or falling or ragdoll) then
-					is_super_running = true
+        if (PAD.IS_CONTROL_PRESSED(2, keys['RB']) and sprinting and not (parastate == 0)) then
+            if not (PED.IS_PED_IN_ANY_VEHICLE(player, true) or falling or ragdoll) then
+                is_super_running = true
 
-					-- Mimic acceleration by increasing speed_mult by sr_accel_mult each tick
-					if (speed_mult <= sr_speed_limit) then
-						speed_mult = speed_mult + sr_accel_mult
-					end
+                -- Mimic acceleration by increasing speed_mult by sr_accel_mult each tick
+                if (speed_mult <= sr_speed_limit) then
+                    speed_mult = speed_mult + sr_accel_mult
+                end
 
-					-- Set up our velocity vector
-					local vx = direction.x * speed_mult
-					local vy = direction.y * speed_mult
-					local vz = 0
+                -- Set up our velocity vector
+                local vx = direction.x * speed_mult
+                local vy = direction.y * speed_mult
+                local vz = 0
 
-					-- Help player stay on ground (kinda) after hitting bumps
-					if (grounddistance > 1 and not swimming) then
-						vz = velocity.z - 2
-					else
-						vz = velocity.z
-					end
+                -- Help player stay on ground (kinda) after hitting bumps
+                if (grounddistance > 1 and not swimming) then
+                    vz = velocity.z - 2
+                else
+                    vz = velocity.z
+                end
 
-					-- Set the players running velocity
-					ENTITY.SET_ENTITY_VELOCITY(player, vx, vy, vz)
-				end
-			else
-				-- Applies the initial braking force to the player at full run when activation button is let go of
-				if (is_super_running and not (falling or ragdoll)) then
-					is_super_running = false
+                -- Set the players running velocity
+                ENTITY.SET_ENTITY_VELOCITY(player, vx, vy, vz)
+            end
+        else
+            -- Applies the initial braking force to the player at full run when activation button is let go of
+            if (is_super_running and not (falling or ragdoll)) then
+                is_super_running = false
 
-					velocity = ENTITY.GET_ENTITY_VELOCITY(player)
-					ENTITY.SET_ENTITY_VELOCITY(player, (velocity.x*sr_brake_mult), (velocity.y*sr_brake_mult), velocity.z)
-				end
+                velocity = ENTITY.GET_ENTITY_VELOCITY(player)
+                ENTITY.SET_ENTITY_VELOCITY(player, (velocity.x*sr_brake_mult), (velocity.y*sr_brake_mult), velocity.z)
+            end
 
-				-- Bring the speed_mult down if the player is not running at all
-				if (speed_mult > sr_base_speed) then
-					speed_mult = speed_mult - sr_brake_mult
-					if (speed_mult < sr_base_speed) then
-						speed_mult = sr_base_speed
-					end
-				end
-			end
-		end
+            -- Bring the speed_mult down if the player is not running at all
+            if (speed_mult > sr_base_speed) then
+                speed_mult = speed_mult - sr_brake_mult
+                if (speed_mult < sr_base_speed) then
+                    speed_mult = sr_base_speed
+                end
+            end
+        end
+        return bSuperRunEnabled
 	end)
 end
 
 function MenuSuperRunSetup(menu_root)
     menu.divider(menu_root, '-------------Speedy\'s Super Run-------------')
 
-    menu.slider(menu_root, 'Speed Limit', {}, '', 0, 200, sr_speed_limit, 1, function(value, prev_value, click_type) 
+    menu.slider(menu_root, 'Speed Limit', {}, '', 0, 200, sr_speed_limit, 1, function(value, prev_value, click_type)
         sr_speed_limit = value
     end)
 
-    menu.slider_float(menu_root, 'Acceleration Multiplyer', {}, '', 0, 1000, sr_accel_mult * 100, 10, function(value, prev_value, click_type) 
+    menu.slider_float(menu_root, 'Acceleration Multiplyer', {}, '', 0, 1000, sr_accel_mult * 100, 10, function(value, prev_value, click_type)
         sr_accel_mult = value / 100
     end)
 
-    menu.slider_float(menu_root, 'Brake Multiplyer', {}, '', 0, 1000, sr_brake_mult * 100, 10, function(value, prev_value, click_type) 
+    menu.slider_float(menu_root, 'Brake Multiplyer', {}, '', 0, 1000, sr_brake_mult * 100, 10, function(value, prev_value, click_type)
         sr_brake_mult = value / 100
     end)
 
