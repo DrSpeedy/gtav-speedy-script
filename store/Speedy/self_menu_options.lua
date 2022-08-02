5fb4bb307afa5a7d3963b31c8f3f8d6266cb21e1
Brian Wilson <brian@wiltech.org>
2022-08-02 05:09:25 -0400

    Messing around w/ aimbot (not working) and added crosshair resources for later

--- store/Speedy/self_menu_options.lua
+++ store/Speedy/self_menu_options.lua
@@ -1,6 +1,8 @@
 -- DrSpeedy#1852
 -- https://github.com/DrSpeedy
 
+local bOnFootAimbotEnabled = false
+
 local function DoSuperJump(toggle)
     util.create_tick_handler(function()
         if (toggle) then
@@ -21,9 +23,65 @@ local function DoSuperJump(toggle)
     end)
 end
 
+local function DoOnFootAimbot(toggle)
+    bOnFootAimbotEnabled = toggle
+    util.create_tick_handler(function()
+        if (PAD.IS_CONTROL_PRESSED(2, keys['LT'])) then
+            local player = PLAYER.PLAYER_PED_ID()
+            local aim_coords = GetOffsetFromCam(80)
+            local target_id = GetClosestPlayerToCoords(aim_coords, 80)
+            if (target_id ~= -1 and target_id ~= PLAYER.PLAYER_ID()) then
+                local target_ped = PLAYER.GET_PLAYER_PED(target_id)
+                local target_coords = ENTITY.GET_ENTITY_COORDS(target_ped)
+
+                if (ENTITY.HAS_ENTITY_CLEAR_LOS_TO_ENTITY_IN_FRONT(player, target_ped)) then
+                    TASK.TASK_TURN_PED_TO_FACE_COORD(player, target_coords.x, target_coords.y, target_coords.z, 1)
+                    TASK.TASK_AIM_GUN_AT_COORD(player, target_coords.x, target_coords.y, target_coords.z, 1, 1, 1)
+                end
+            end
+        end
+        return bOnFootAimbotEnabled
+    end)
+end
+
+-- Quick Teleports
+local function DoTeleportForward()
+    local player = PLAYER.PLAYER_PED_ID()
+    local coords = ENTITY.GET_ENTITY_COORDS(player)
+    local direction = ENTITY.GET_ENTITY_FORWARD_VECTOR(player)
+    local distance = 4
+
+    local x = coords.x + (direction.x * distance)
+    local y = coords.y + (direction.y * distance)
+    local z = coords.z + (direction.z * distance)
+
+    ENTITY.SET_ENTITY_COORDS(player, x, y, z)
+end
+
+local function DoTeleportUpward()
+    local player = PLAYER.PLAYER_PED_ID()
+    local coords = ENTITY.GET_ENTITY_COORDS(player)
+    local distance = 10
+
+    local x = coords.x
+    local y = coords.y
+    local z = coords.z + distance
+
+    ENTITY.SET_ENTITY_COORDS(player, x, y, z)
+end
+
+local function MenuQuickTeleSetup(menu_root)
+    menu.action(menu_root, 'Teleport Forward', {}, '', function() DoTeleportForward() end)
+    menu.action(menu_root, 'Teleport Upward', {}, '', function() DoTeleportUpward() end)
+end
+
 function MenuSelfSetup(menu_root)
     -- Self Menu
     menuIdTbl['Self.Super Run'] = menu.list(menu_root, 'Super Run', {}, '')
     menuIdTbl['Self.Super Flight'] = menu.list(menu_root, 'Super Flight', {}, '')
+    menuIdTbl['Self.Quick Teleport'] = menu.list(menu_root, 'Quick Teleport', {}, '')
+    menu.toggle(menu_root, 'Aimbot', {}, '', function(toggle) DoOnFootAimbot(toggle) end)
     menu.toggle(menu_root, 'Super Jump', {}, '', function(toggle) DoSuperJump(toggle) end)
+
+    MenuQuickTeleSetup(menuIdTbl['Self.Quick Teleport'])
 end
